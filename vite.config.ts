b166afdd9b9817dd39f04f7c0a8a940e0950726f
nicolocko <massonreedservices@gmail.com>
2026-07-26 14:00:07 +0200

    fix: vercel output directory

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [devtools(), tailwindcss(), tanstackStart({ deployment: { preset: 'vercel' } }), viteReact()],
+  build: { chunkSizeWarningLimit: 1000 },
 })
 
 export default config
